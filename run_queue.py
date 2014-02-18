e0964872d4a41a4f31e9d666ad5cfcb131e3f27e
Eric van Zanten <eric.vanzanten@gmail.com>
2014-02-18 17:28:32 -0600

    95% finished. Just need to figure out the async piece at the end

--- run_queue.py
+++ run_queue.py
@@ -1,12 +1,25 @@
-from multiprocessing.connection import Listener
+from multiprocessing.connection import Listener, Client
+from multiprocessing import Pool
 import dedupe
 from dedupe.serializer import _to_json
+from dedupe import AsciiDammit
 from uuid import uuid4
 import copy
 import os
+import re
+import logging
+from cStringIO import StringIO
+import csv
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'upload_data')
 dedupers = {}
+
+TRAINING_RECV = ('localhost', 6000)
+DEDUPE_RECV = ('localhost', 6200)
+
 
 def preProcess(column):
     column = AsciiDammit.asciiDammit(column)
@@ -24,85 +37,155 @@ def readData(f):
         data[row_id] = dedupe.core.frozendict(clean_row)
     return data
 
-def dedupe_loop(conn):
-    while True:
-        msg = conn.recv()
+def writeResults(inp, file_path, clustered_dupes):
+
+    # Write our original data back out to a CSV with a new column called 
+    # 'Cluster ID' which indicates which records refer to each other.
+
+    logging.info('saving results to: %s' % file_path)
+
+    cluster_membership = collections.defaultdict(lambda : 'x')
+    for cluster_id, cluster in enumerate(clustered_dupes):
+        for record_id in cluster:
+            cluster_membership[record_id] = cluster_id
+
+    writer = csv.writer(open(file_path, 'wb'))
+
+    reader = csv.reader(StringIO(inp))
+
+    heading_row = reader.next()
+    heading_row.insert(0, 'Cluster ID')
+    writer.writerow(heading_row)
+
+    for i, row in enumerate(reader):
+        row_id = i
+        cluster_id = cluster_membership[row_id]
+        row.insert(0, cluster_id)
+        writer.writerow(row)
+
+# ## Writing results
+def writeUniqueResults(inp, file_path, clustered_dupes):
+
+    # Write our original data back out to a CSV with a new column called 
+    # 'Cluster ID' which indicates which records refer to each other.
+
+    logging.info('saving unique results to: %s' % file_path)
+
+    cluster_membership = {}
+    for (cluster_id, cluster) in enumerate(clustered_dupes):
+        logging.info(cluster)
+        for record_id in cluster:
+            cluster_membership[record_id] = cluster_id
+
+    writer = csv.writer(open(file_path, 'wb'))
+
+    reader = csv.reader(StringIO(inp))
+
+    heading_row = reader.next()
+    writer.writerow(heading_row)
+
+    seen_clusters = set()
+    for i, row in enumerate(reader):
+        row_id = i
+        if row_id in cluster_membership: 
+            cluster_id = cluster_membership[row_id]
+            if cluster_id not in seen_clusters:
+                writer.writerow(row)
+                seen_clusters.add(cluster_id)
+        else:
+            writer.writerow(row)
+
+def training_loop(msg):
+    deduper_id = msg.get('deduper_id')
+    if msg.get('step') == 'finish':
         deduper_id = msg['deduper_id']
-        deduper = dedupers[deduper_id]
+        deduper = dedupers[deduper_id]['deduper']
         deduper.train()
         filename = dedupers[deduper_id]['filename']
         settings_path = os.path.join(UPLOAD_FOLDER, '%s-settings.dedupe' % filename)
         training_path = os.path.join(UPLOAD_FOLDER, '%s-training.json' % filename)
         deduper.writeTraining(training_path)
         deduper.writeSettings(settings_path)
-
-def training_loop(conn):
-    while True:
-        msg = conn.recv()
-        deduper_id = msg.get('deduper_id')
-        if msg.get('step') == 'done':
-            dedupe_conn = Client(('localhost', 6002))
-            dedupe_conn.send(deduper_id)
-            dedupe_conn.close()
-        elif msg.get('step') is 1:
-            # Establish the key and save the CSV
-            dedupers[deduper_id] = {
-                'csv': msg['csv'],
-                'filename': msg['filename']
-            }
-        elif msg.get('step') is 2:
-            # Save field_defs and make sample
-            field_defs = {}
-            field_list = msg['field_list']
-            for field in field_list:
-                field_defs[field] = {'type': 'String'}
-            dedupers[deduper_id]['field_defs'] = copy.deepcopy(field_defs)
-            dedupers[deduper_id]['deduper'] = dedupe.Dedupe(field_defs)
-            deduper.sample(data_d, 150000)
-        elif msg.get('step') == 'get_pair':
-            deduper = dedupers[deduper_id]['deduper']
-            fields = deduper.data_model.comparison_fields
-            record_pair = deduper.getUncertainPair()[0]
-            client = Client(('localhost', 6001), authkey=deduper_id)
-            client.send(record_pair)
-            client.close()
-        elif msg.get('step') == 'mark_pair':
-            current_pair = dedupers[deduper_id]['current_pair']
-            action = msg.get('action')
-            if dedupers[deduper_id].get('counter'):
-                counter = dedupers[deduper_id]['counter']
-            else:
-                counter = {'yes': 0, 'no': 0, 'unsure': 0}
-            if dedupers[deduper_id].get('training_data'):
-                labels = dedupers[deduper_id]['training_data']
-            else:
-                labels = {'distinct' : [], 'match' : []}
-            if action == 'yes':
-                labels['match'].append(current_pair)
-                counter['yes'] += 1
-                resp = {'counter': counter}
-            elif action == 'no':
-                labels['distinct'].append(current_pair)
-                counter['no'] += 1
-                resp = {'counter': counter}
-            else:
-                counter['unsure'] += 1
-                dedupers[deduper_id]['counter'] = counter
-                resp = {'counter': counter}
-            deduper = dedupers[deduper_id]['deduper']
-            deduper.markPairs(labels)
-            dedupers[deduper_id]['training_data'] = labels
+        threshold = self.deduper.threshold(self.data_d, recall_weight=2)
+        clustered_dupes = self.deduper.match(self.data_d, threshold)
+        logging.info('clustering done')
+        deduped_unique_file_path = os.path.join(UPLOAD_FOLDER, '%s-deduped_unique.csv' % filename)
+        raw = dedupers[deduper_id]['csv']
+        writeUniqueResults(raw, deduped_unique_file_path, clustered_dupes)
+        deduped_file_path = os.path.join(UPLOAD_FOLDER, '%s-deduped.csv' % filename)
+        writeResults(raw, deduped_file_path, clustered_dupes)
+        client = Client(('localhost', msg['port']), authkey=deduper_id)
+        resp = {
+            'training': os.path.relpath(training_path, __file__),
+            'settings': os.path.relpath(settings_path, __file__),
+            'deduped_unique': os.path.relpath(deduped_unique_file_path, __file__),
+            'deduped': os.path.relpath(deduped_file_path, __file__),
+        }
+        client.send(resp)
+        client.close()
+    elif msg.get('step') is 1:
+        # Establish the key and save the CSV
+        dedupers[deduper_id] = {
+            'csv': msg['csv'],
+            'filename': msg['filename']
+        }
+    elif msg.get('step') is 2:
+        # Save field_defs and make sample
+        field_defs = {}
+        field_list = msg['field_list']
+        for field in field_list:
+            field_defs[field] = {'type': 'String'}
+        dedupers[deduper_id]['field_defs'] = copy.deepcopy(field_defs)
+        dedupers[deduper_id]['deduper'] = dedupe.Dedupe(field_defs, num_processes=8)
+        inp = StringIO(dedupers[deduper_id]['csv'])
+        dedupers[deduper_id]['data_d'] = readData(inp)
+        dedupers[deduper_id]['deduper'].sample(dedupers[deduper_id]['data_d'], 150000)
+    elif msg.get('step') == 'get_pair':
+        deduper = dedupers[deduper_id]['deduper']
+        fields = deduper.data_model.comparison_fields
+        record_pair = deduper.getUncertainPair()[0]
+        dedupers[deduper_id]['current_pair'] = record_pair
+        client = Client(('localhost', msg['port']), authkey=deduper_id)
+        client.send((record_pair, fields))
+        client.close()
+    elif msg.get('step') == 'mark_pair':
+        current_pair = dedupers[deduper_id]['current_pair']
+        action = msg.get('action')
+        if dedupers[deduper_id].get('counter'):
+            counter = dedupers[deduper_id]['counter']
+        else:
+            counter = {'yes': 0, 'no': 0, 'unsure': 0}
+        if dedupers[deduper_id].get('training_data'):
+            labels = dedupers[deduper_id]['training_data']
+        else:
+            labels = {'distinct' : [], 'match' : []}
+        if action == 'yes':
+            labels['match'].append(current_pair)
+            counter['yes'] += 1
+            resp = {'counter': counter}
+        elif action == 'no':
+            labels['distinct'].append(current_pair)
+            counter['no'] += 1
+            resp = {'counter': counter}
+        else:
+            counter['unsure'] += 1
             dedupers[deduper_id]['counter'] = counter
-            conn.send(resp)
+            resp = {'counter': counter}
+        deduper = dedupers[deduper_id]['deduper']
+        deduper.markPairs(labels)
+        dedupers[deduper_id]['training_data'] = labels
+        dedupers[deduper_id]['counter'] = counter
+    return 'Step %s done: %s' % (msg['step'], msg['deduper_id'])
 
 if __name__ == "__main__":
-    training_address = ('localhost', 6000)
-    training_listener = Listener(training_address)
-    training_conn = training_listener.accept()
-    training_loop(training_conn)
+    training_listener = Listener(TRAINING_RECV)
+    while True:
+        training_conn = training_listener.accept()
+        try:
+            msg = training_conn.recv()
+            training_loop(msg)
+        except EOFError:
+            continue
 
-    dedupe_address = ('localhost', 6002)
-    dedupe_listener = Listener(dedupe_address)
-    dedupe_conn = dedupe_listener.accept()
-    dedupe_loop(dedupe_conn)
+    # dedupe_loop(DEDUPE_RECV)
 
