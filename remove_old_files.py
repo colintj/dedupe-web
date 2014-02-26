d0c1f916f1abb4a5253e1bec0e7da6eac32e2992
Eric van Zanten <eric.vanzanten@gmail.com>
2014-02-26 20:11:44 +0000

    Fix file removal script

--- remove_old_files.py
+++ remove_old_files.py
@@ -3,15 +3,18 @@ import time
 
 def remove_oldies(folder):
     now = time.time()
-    then = now - (60 * 60)
+    then = now - (60 * 15)
+    nuked = []
     for f in os.listdir(folder):
         ts = f.split('_')[0]
         try:
             if float(ts) < then:
+                nuked.append(f)
                 os.remove(os.path.join(folder, f))
         except ValueError:
             pass
     return nuked
 
 if __name__ == "__main__":
-    remove_oldies('upload_data')
+    curdir = os.path.dirname(__file__)
+    remove_oldies(os.path.join(curdir, 'upload_data'))
